02c49dda75e419f72f25ceb93131edd9ae4e38c6
unknown <shidonosan@gmail.com>
2025-12-01 15:51:29 -0500

    fix: index & about layout fix

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -3,16 +3,16 @@ import { i18n } from "@i18n/translation";
 import { LinkPreset, type NavBarLink } from "@/types/config";
 
 export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
-  [LinkPreset.About]: {
-    name: i18n(I18nKey.about),
-    url: "/",
-  },
-  [LinkPreset.Experience]: {
-    name: i18n(I18nKey.experience),
-    url: "/experiences/",
-  },
-  [LinkPreset.Project]: {
-    name: i18n(I18nKey.projects),
-    url: "/projects/",
-  },
+	[LinkPreset.About]: {
+		name: i18n(I18nKey.about),
+		url: "/about/",
+	},
+	[LinkPreset.Experience]: {
+		name: i18n(I18nKey.experience),
+		url: "/experiences/",
+	},
+	[LinkPreset.Project]: {
+		name: i18n(I18nKey.projects),
+		url: "/projects/",
+	},
 };
